dc5cbbd7fa5b94596fcc93ecde73755afb482066
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-25 13:31:36 -0600

    Lab with a countdown timer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // add useEffect code. the code 
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return; // exit early!
+    }
+
+    // set up a timeout to run after 1 second
+    const timerId = setTimeout(() => {
+      // decrement the time remaining
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    // clean up after the timeout in case the component unmounts before the timer is done
+    return function () {
+      clearTimeout(timerId);
+    };
+  }, [timeRemaining, onAnswered]);
+
+
+  //   useEffect(() => {
+  //     timeRemaining === 0
+  //       ? (
+  //         setTimeRemaining(10),
+  //         onAnswered(false)
+  //       )
+  //       : (
+  //         // Corrected: Move timerId outside the ternary block
+  //         const timerId = setTimeout(() => {
+  //         setTimeRemaining((timeRemaining) => timeRemaining - 1);
+  //       }, 1000);
+
+  //     // Return a cleanup function
+  //     return function () {
+  //       clearTimeout(timerId);
+  //     }
+  //       );
+
+  // }, [timeRemaining, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
